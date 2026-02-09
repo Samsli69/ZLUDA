46b429d84a6bb4f3d4ee4c3d58042090be7d257e
Andrzej Janik <vosen@vosen.pl>
2026-02-08 18:24:42 -0800

    Formatting

--- zluda_blas/build.rs
+++ zluda_blas/build.rs
@@ -1,4 +1,6 @@
 extern crate embed_resource;
 fn main() {
-    embed_resource::compile("manifest.rc", embed_resource::NONE).manifest_optional().unwrap();
+    embed_resource::compile("manifest.rc", embed_resource::NONE)
+        .manifest_optional()
+        .unwrap();
 }

b201771c2d14d0a2309c31d1b2ecdf2e1a35c868
Andrzej Janik <vosen@vosen.pl>
2026-02-08 18:25:44 -0800

    Fix Linux build

--- zluda_blas/build.rs
+++ zluda_blas/build.rs
@@ -1,6 +1,10 @@
-extern crate embed_resource;
+#[cfg(windows)]
 fn main() {
+    extern crate embed_resource;
     embed_resource::compile("manifest.rc", embed_resource::NONE)
         .manifest_optional()
         .unwrap();
 }
+
+#[cfg(not(windows))]
+fn main() {}
